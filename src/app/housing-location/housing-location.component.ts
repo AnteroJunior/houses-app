c6d151dadfd2b4acfd26327cb702417040cbe277
Antero Júnior <anterojunior1530@gmail.com>
2024-01-06 13:28:54 -0300

    feat: Creating new component (housingLocation) and importing to HomeComponent

---
+++ src/app/housing-location/housing-location.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-housing-location',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './housing-location.component.html',
+  styleUrl: './housing-location.component.css'
+})
+export class HousingLocationComponent {
+
+}
